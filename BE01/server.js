51552029b8edc4dfd1f1e92beefa220039b9410c
J0sH-code <joshrylesanteno07@gmail.com>
2026-07-12 22:10:19 +0800

    Added express dependency

---
+++ BE01/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+    res.sendStatus(200).json({
+        message: "Hello from Josh Santeno",
+        description: "First endpoint from week 1 assignment"
+    })
+})
+
+app.get("/code", (req, res) => {
+    res.sendStatus(200).json({
+        message: "Second endpoint from week 1 assignment"
+    })
+})
+
+app.listen(port,() => {
+    console.log(`Listening to ${port}`)
+})
