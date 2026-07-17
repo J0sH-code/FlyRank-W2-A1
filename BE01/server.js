cdce8bad2cf6ad9f449bac98f36fad8fc013f4d2
J0sH-code <joshrylesanteno07@gmail.com>
2026-07-17 23:42:35 +0800

    Stage 4: full CRUD

--- BE01/server.js
+++ BE01/server.js
@@ -61,6 +61,38 @@ app.post("/tasks", (req, res) => {
     })
 })
 
+app.put("/tasks/:id", (req, res) => {
+    if (req.body == null || (req.body.title == null && req.body.done == null)) {
+        res.status(400).json({
+            error: "Invalid/missing request body"
+        })
+    }
+
+    const requestedTask = tasks.find(task => task.id == req.params.id)
+    if (requestedTask == null) {
+        res.status(404).json({
+            error: `Task ${req.params.id} not found`
+        })
+    }
+
+    requestedTask.done = req.body.done;
+    requestedTask.title = req.body.title;
+
+    res.status(201).send(requestedTask)
+})
+
+app.delete("/tasks/:id", (req, res) => {
+    const requestedTask = tasks.find(task => task.id == req.params.id)
+    if (requestedTask == null) {
+        res.status(404).json({
+            error: `Task ${req.params.id} not found`
+        })
+    }
+
+    tasks.splice(tasks.indexOf(requestedTask),1)
+    res.sendStatus(204)
+})
+
 app.listen(port,() => {
     console.log(`Listening to ${port}`)
 })
