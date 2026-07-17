c6bb073e97c3fcd18994a1bdc9f2a8f92e5c2d4c
J0sH-code <joshrylesanteno07@gmail.com>
2026-07-17 22:59:18 +0800

    Stage 2: read endpoints with 404

--- BE01/server.js
+++ BE01/server.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express()
 const port = 3000
 
+const tasks = [
+    {id: 123, title: "Note1", done:true},
+    {id: 234, title: "Note2", done:true},
+    {id: 345, title: "Note3", done:true}
+]
+
 app.get("/", (req, res) => {
     res.status(200).json({
         name: "Task API",
@@ -15,6 +21,20 @@ app.get("/health", (req, res) => {
     res.status(200).json({
         status: "ok"
     })
+})
+
+app.get("/tasks", (req, res) => {
+    res.status(200).send(tasks)
+})
+
+app.get("/tasks/:id", (req, res) => {
+    const requestedTask = tasks.find(task => task.id == req.params.id)
+    if (requestedTask == null) {
+        res.status(404).json({
+            error: `Task ${req.params.id} not found`
+        })
+    }
+    res.status(200).send(requestedTask)
 })
 
 app.listen(port,() => {
